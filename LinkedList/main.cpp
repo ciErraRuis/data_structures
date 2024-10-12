11062e0a2e4c48d1458b71781f3e424430060464
cierra runis <594553306@qq.com>
2024-10-13 05:46:36 +0800

    fix some problem

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include "LinkedList.h"
 
@@ -32,16 +31,3 @@ int main() {
     c1.printList();    
     c2.printList(); //测试【删除】操作是否正确，每种情况6分return0;
 }
-=======
-#include "LinkedList.h"
-
-int main()
-{
-    SingleLinkedList<int> a{1,2,3,4,5};int a_get = a.getCurrentVal();    std::cout << a_get << std::endl;
-    SingleLinkedList<char> b{'a','c','e'};    b.setCurrentVal('g');    b.printList();
-    SingleLinkedList<char> e;    std::cout << b.isEmpty() << " "              << e.isEmpty() << std::endl; //测试【空链表判断】操作是否正确，18分
-    SingleLinkedList<double> c1;    SingleLinkedList<double> c2{1.5,2.0,9.0,4.7};    SingleLinkedList<double> c3;    c3 = c2;    c1.insert(3.0);    c2.insert(8.8);bool f_c3 = c3.find(2.0);    c3.insert(8.8);    c1.printList();    c2.printList();    c3.printList(); //测试【插入】操作是否正确，每种情况6分
-    e.remove();    c1.remove();bool f = c2.find(9.0);    c2.remove();    e.printList();    c1.printList();    c2.printList(); //测试【删除】操作是否正确，每种情况6分
-    return 0;
-};
->>>>>>> 55e02fbc805478c1406774f34829f6856dbe5999
